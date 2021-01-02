1f83c69e7b63bb4d9f290a6cdb346656a542d28b
George Ponick <gponick@gmail.com>
2021-01-02 17:17:21 -0500

    Adding NXT

--- vkb/devices/defs.py
+++ vkb/devices/defs.py
@@ -1,4 +1,5 @@
 from .base import VENDOR_ID
 from .gladiatork import GladiatorK, GladiatorKLH
+from .gladiatornxt import GladiatorNXT
 
-VKB_DEVICES = {0x0132: GladiatorK, 0x0133: GladiatorKLH}
+VKB_DEVICES = {0x0132: GladiatorK, 0x0133: GladiatorKLH, 0x0200: GladiatorNXT}
